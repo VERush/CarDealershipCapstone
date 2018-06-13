19a468870e688a04b5de64b0b879e34e5557e627
VERush <Vicki@rushworld.com>
2018-06-13 10:01:43 -0400

    Added bitMask logic

--- src/main/java/com/finalcapstone/CarDealershipCapstone/RestCarController.java
+++ src/main/java/com/finalcapstone/CarDealershipCapstone/RestCarController.java
@@ -1,5 +1,10 @@
 package com.finalcapstone.CarDealershipCapstone;
-
+/*
+ * Car Dealership Capstone
+ * Showcasing API creation
+ * Allison Farr, Ben McBrayer, Jonah Wooten, Victoria Rush
+ * April 2018 Grand Circus Java Bootcamp
+ */
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,87 +24,58 @@ public class RestCarController {
 	@GetMapping("/carlist")
 	public List<Car> getCarList(@RequestParam(value = "make", required = false) String make,
 			@RequestParam(value = "model", required = false) String model,
-			@RequestParam(value = "year", required = false) int year,
+			@RequestParam(value = "year", required = false) String year,
 			@RequestParam(value = "color", required = false) String color) {
+		int bitMask = 0;
 
-		if (make.isEmpty()) {
-			if (model.isEmpty()) {
-				if (year < 1000) {
-					return cr.findByColor(color);
-				} else
-					return cr.findByYearAndColor(year, color);
-			} else
-				return cr.findByModelAndYearAndColor(model, year, color);
+		if (!make.isEmpty()) {
+			bitMask+=16;
 		}
-
-		if (model.isEmpty()) {
-			if (year < 1000) {
-				if (color.isEmpty())
-					return cr.findByMake(make);
-			} else
-				return cr.findByMakeAndColor(make, color);
-		} else
-			return cr.findByMakeAndYearAndColor(make, year, color);
-
-		if (year < 1000) {
-			if (color.isEmpty()) {
-				if (make.isEmpty()) {
-					return cr.findByModel(model);
-				} else
-					return cr.findByMakeAndModel(make, model);
-			} else
-				return cr.findByMakeAndModelAndColor(make, model, color);
+			
+		if (!model.isEmpty()) {
+			bitMask+=8;
 		}
-
-		if (color.isEmpty()) {
-			if (make.isEmpty()) {
-				if (model.isEmpty()) {
+		
+		if (year > 1000) {
+			bitMask+=4;	
+		}
+		
+		if (!color.isEmpty()) {
+			bitMask+=2;	
+		}
+		
+			switch (bitMask) {
+			case 2: 
+				return cr.findByColor(color);
+			case 4:
 					return cr.findByYear(year);
-				} else
-					return cr.findByModelAndYear(model, year);
-			} else
+			case 6: 
+					return cr.findByYearAndColor(year, color);
+			case 8: 
+				return cr.findByModel(model);
+			case 10:
+				return cr.findByModelAndColor(model, color);
+			case 12:
+				return cr.findByModelAndYear(model, year);
+			case 14:
+				return cr.findByModelAndYearAndColor(model, year, color);
+			case 16:
+				return cr.findByMake(make);
+			case 18:
+				return cr.findByMakeAndColor(make, color);
+			case 20:
+				return cr.findByMakeAndYear(make, year);
+			case 22:
+				return cr.findByMakeAndYearAndColor(make, year, color);
+			case 24:
+				return cr.findByMakeAndModel(make, model);
+			case 26:
+				return cr.findByMakeAndModelAndColor(make, model, color);
+			case 28:
 				return cr.findByMakeAndModelAndYear(make, model, year);
-		}
-
-		if ((!make.isEmpty()) && (year > 1000) && (model.isEmpty()) && (color.isEmpty())) {
-			return cr.findByMakeAndYear(make, year);
-		}
-
-		if ((!model.isEmpty()) && (!color.isEmpty()) && (make.isEmpty()) && (year < 1000)) {
-			return cr.findByModelAndColor(model, color);
-		}
-
-		else return cr.findByMakeAndModelAndYearAndColor(make, model, year, color);
+			default:
+				return cr.findByMakeAndModelAndYearAndColor(make, model, year, color);
+			}	
 	}
-
-	@GetMapping("/carmake")
-	public List<Car> carMake(@RequestParam("make") String make) {
-
-		return cr.findByMake(make);
-	}
-
-	@GetMapping("/carmodel")
-	public List<Car> carModel(@RequestParam("model") String model) {
-
-		return cr.findByModel(model);
-	}
-
-	@GetMapping("/caryear")
-	public List<Car> carYear(@RequestParam("year") int year) {
-
-		return cr.findByYear(year);
-	}
-
-	@GetMapping("/carcolor")
-	public List<Car> carColor(@RequestParam("color") String color) {
-
-		return cr.findByColor(color);
-	}
-
-	@GetMapping("/carmakemodel")
-	public List<Car> carColor(@RequestParam("make") String make, @RequestParam("model") String model) {
-
-		return cr.findByMakeAndModel(make, model);
-	}
-
+		
 }
